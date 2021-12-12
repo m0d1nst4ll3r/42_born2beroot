3c988750ce979f96162eeb8e0a9055bfebaa2ff5
m0d1nst4ll3r <raphaelpohlen@gmail.com>
2021-12-12 22:22:01 +0100

    improved monitoring.sh fixed crontab scheduling error

--- monitoring.sh
+++ monitoring.sh
@@ -12,20 +12,20 @@ if [ $(lsblk | grep "lvm" | wc -l) -gt "0" ]
 then echo "yes"
 else echo "no"
 fi)
-M_TCP=$(ss -s | grep estab | awk '{print $4}' | tr -d , && echo ESTABLISHED)
+M_TCP=$(ss -s | grep estab | awk '{print $4}' | tr -d ',\n' && echo ' ESTABLISHED')
 M_USER=$(who | awk '{print $1}' | sort | uniq | wc -l)
 M_IP=$(ip a | grep 'ether\|global' | awk '{print $2}' | sed 's/\/.*//' | tr '\n' ' ' | awk '{printf "IP %s (%s)", $2, $1}')
 M_SUDO=$(cat /var/log/sudo/logs | grep -c "COMMAND" && echo " cmd")
 
-echo -n "#Architecture	: "; echo $M_ARCH
-echo -n "#CPU Physical	: "; echo $M_CPU
-echo -n "#vCPU		: "; echo $M_VCPU
-echo -n "#Memory Usage	: "; echo $M_RAM
-echo -n "#Disk Usage	: "; echo $M_DISK
-echo -n "#CPU Load	: "; echo $M_LOAD
-echo -n "#Last Boot	: "; echo $M_BOOT
-echo -n "#LVM Use	: "; echo $M_LVM
-echo -n "#TCP Connections: "; echo $M_TCP
-echo -n "#User Log	: "; echo $M_USER
-echo -n "#Network	: "; echo $M_IP
-echo -n "#Sudo		: "; echo $M_SUDO
+echo "#Architecture	: ${M_ARCH}"
+echo "#CPU Physical	: ${M_CPU}"
+echo "#vCPU		: ${M_VCPU}"
+echo "#Memory Usage	: ${M_RAM}"
+echo "#Disk Usage	: ${M_DISK}"
+echo "#CPU Load	: ${M_LOAD}"
+echo "#Last Boot	: ${M_BOOT}"
+echo "#LVM Use	: ${M_LVM}"
+echo "#TCP Connections: ${M_TCP}"
+echo "#User Log	: ${M_USER}"
+echo "#Network	: ${M_IP}"
+echo "#Sudo		: ${M_SUDO}"
